d5574c3e86d73f46ac29ef56b169e01fbd9b8016
Micheal Quentin <mquenti3@gmail.com>
2021-03-30 12:26:35 -0500

    Editor Window Displays Names
    The editor script loads the object names from the scriptable object and display a list of the names in the validator window.

--- Task2/Assets/Editor/EditorScriptExample.cs
+++ Task2/Assets/Editor/EditorScriptExample.cs
@@ -1,13 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
 
 public class EditorScriptExample : EditorWindow
 {
+    public ScriptableObjectExample scriptableObject;
+
     [MenuItem("Window/Validator")]
     public static void ShowWindow()
     {
         GetWindow<EditorScriptExample>("Validator");
+    }
+
+    private void OnEnable()
+    {
+        scriptableObject = Resources.Load<ScriptableObjectExample>("ScriptableObjects/GameObjects");
+    }
+
+    private void OnGUI()
+    {
+        for (int i = 0; i < scriptableObject.objectNames.Count; i++)
+        {
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.Label(scriptableObject.objectNames[i]);
+            EditorGUILayout.EndHorizontal();
+        }
     }
 }
---
+++ Task2/Assets/Resources/Scripts/ScriptableObjectExample.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New ScriptableObject", menuName = "ScriptableObject")]
+public class ScriptableObjectExample : ScriptableObject
+{
+    public List<string> objectNames = new List<string>();
+}
