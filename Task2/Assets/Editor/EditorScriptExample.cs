7269620a28dfb51752b74ae6e3af083e2c12fa18
Micheal Quentin <mquenti3@gmail.com>
2021-03-30 12:13:26 -0500

    New Window Created
    A window can be opened from the editor

---
+++ Task2/Assets/Editor/EditorScriptExample.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EditorScriptExample : EditorWindow
+{
+    [MenuItem("Window/Validator")]
+    public static void ShowWindow()
+    {
+        GetWindow<EditorScriptExample>("Validator");
+    }
+}
